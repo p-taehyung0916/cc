c88164f39097ee5193f60207090c9790b815662d
shipon <p-taehyung@pro-vision.jp>
2024-09-13 18:26:40 +0900

    hi

---
+++ src/app/Control.js
@@ -0,0 +1,38 @@
+'use client';
+import Link from 'next/link';
+import { useParams, useRouter } from 'next/navigation';
+
+export function Control() {
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  return (
+    <ul>
+      <li>
+        <Link href="/create">Create</Link>
+      </li>
+      {id ? (
+        <>
+          <li>
+            <Link href={'/update/' + id}>Update</Link>
+          </li>
+          <li>
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: 'DELETE' };
+                fetch(process.env.NEXT_PUBLIC_API_URL + 'topics/' + id, options)
+                  .then((resp) => resp.json())
+                  .then((result) => {
+                    router.push('/');
+                    router.refresh();
+                  });
+              }}
+            ></input>
+          </li>
+        </>
+      ) : null}
+    </ul>
+  );
+}
